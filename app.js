88b886c6a9604818e19728ec5340ee985b88403a
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-02-11 17:09:52 -0600

    score display resets properly with button

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 // keeps track of player One's score //
 let p1Score = 0;
@@ -21,6 +22,8 @@ p1Button.addEventListener('click', () => {
       // checking score for the win //
       if(p1Score === winningScore){
          isGameOver = true;
+         p1Display.classList.add('winner');
+         p2Display.classList.add('loser');
       }
       p1Display.textContent = p1Score;
    }
@@ -34,17 +37,28 @@ p2Button.addEventListener('click', () => {
       // checking score for the win //
       if(p2Score === winningScore){
          isGameOver = true;
+         p2Display.classList.add('winner');
+         p1Display.classList.add('loser');
       }
       p2Display.textContent = p2Score;
    }
 })
 
+// add event listener to winningScoreSelect to listen for a change event //
+winningScoreSelect.addEventListener('change', function () {
+   winningScore = parseInt(this.value);
+   reset();
+})
+
 // Add EventListener to reset button to reset the score when clicked //
-resetButton.addEventListener('click', () => {
+resetButton.addEventListener('click', reset);
+
+function reset () {
    isGameOver = false;
    p1Score = 0;
    p2Score = 0;
    p1Display.textContent = 0;
    p2Display.textContent = 0;
-
-})
+   p1Display.classList.remove('winner', 'loser');
+   p2Display.classList.remove('winner', 'loser');
+}
