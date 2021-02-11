ff5f59c30f4eec26f12c438217795f82ba35191c
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-02-11 16:29:38 -0600

    reset score button working

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
@@ -9,20 +10,41 @@ let p1Score = 0;
 let p2Score = 0;
 // keeps track of how high the score goes
 let winningScore = 5;
+let isGameOver = false;
 
 
 // Add EventListener to the button for player 1 //
 p1Button.addEventListener('click', () => {
-   if (p1Score !== winningScore){
+   if(!isGameOver){ // if the game is still being played //
+      // add 1 to player 1's score //
       p1Score += 1;
+      // checking score for the win //
+      if(p1Score === winningScore){
+         isGameOver = true;
+      }
       p1Display.textContent = p1Score;
    }
 })
 
 // Add EventListener to the button for player 2 //
 p2Button.addEventListener('click', () => {
-   if (p2Score !== winningScore){
+   if(!isGameOver){ // if the game is still being played //
+      // add 1 to player 2's score //
       p2Score += 1;
+      // checking score for the win //
+      if(p2Score === winningScore){
+         isGameOver = true;
+      }
       p2Display.textContent = p2Score;
    }
+})
+
+// Add EventListener to reset button to reset the score when clicked //
+resetButton.addEventListener('click', () => {
+   isGameOver = false;
+   p1Score = 0;
+   p2Score = 0;
+   p1Display.textContent = 0;
+   p2Display.textContent = 0;
+
 })
